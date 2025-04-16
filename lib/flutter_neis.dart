95c3e64d44bc244b474a6f5f90dd347992143b7e
jinhyeon-dev <jinhyeon.dev@gmail.com>
2025-04-16 14:44:54 +0900

    feat: flutter_neis 수정

--- lib/flutter_neis.dart
+++ lib/flutter_neis.dart
@@ -2,9 +2,11 @@ library flutter_neis;
 
 export 'src/neis_client.dart';
 
+export 'src/neis.dart';
+
 export 'src/models/school_info.dart';
 
-export 'src/neis.dart';
+export 'src/services/school_service.dart';
 
 export 'src/models/meal_info.dart';
 
